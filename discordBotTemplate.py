4a6a80ade5c6d517d1d5ad5dc484bf8a890130a6
Atomic8oy <tpaix87@gmail.com>
2023-07-22 18:26:58 +0330

    Add files via upload

---
+++ discordBotTemplate.py
@@ -0,0 +1,20 @@
+import interactions
+from interactions import slash_command, SlashContext
+
+# Your bot's Activity // Delete this line if you dont want anything
+bot = interactions.Client(activity="ACTIVITY")
+
+# Creating your first slash command
+# name is your command's name [/command-name]
+@slash_command(name="my_Command", description="my_Command's Description") 
+async def my_Command_function(ctx: SlashContext): # - make sure to change [your command's name]_function here -
+    await ctx.send(f"Hello world") # Your command's output is going here
+    print("Someone used /my_Command") # This part is for log its not necessary
+
+
+
+@interactions.listen()
+async def on_startup():
+    print("Bot is ready!")
+
+bot.start("YOUR-BOT-TOKEN") # Make sure to change this part to your bot's token
