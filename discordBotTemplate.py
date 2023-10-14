7a16033869f252f689fa11e2323fcf57615561c6
Atomic8oy <tpaix87@gmail.com>
2023-10-14 22:03:55 +0330

    Update discordBotTemplate.py

--- discordBotTemplate.py
+++ discordBotTemplate.py
@@ -1,13 +1,13 @@
 import interactions
 from interactions import slash_command, SlashContext
 
-# Your bot's Activity // Delete this line if you dont want anything
+# Your bot's Activity // remove "activity=" if you dont want any
 bot = interactions.Client(activity="ACTIVITY")
 
 # Creating your first slash command
-# name is your command's name [/command-name]
-@slash_command(name="my_Command", description="my_Command's Description") 
-async def my_Command_function(ctx: SlashContext): # - make sure to change [your command's name]_function here -
+# name is your command's name [/command-name] 
+@slash_command(name="my_command", description="my_Command's Description") 
+async def my_command_function(ctx: SlashContext): # - make sure to change [your command's name]_function here -
     await ctx.send(f"Hello world") # Your command's output is going here
     print("Someone used /my_Command") # This part is for log its not necessary
 
